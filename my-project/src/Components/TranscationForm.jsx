38edbf71b988931c50d2a328dc890dd458203d4e
Phumipat <158813193+phumipatsrisu@users.noreply.github.com>
2026-03-08 16:37:19 +0700

    Update TranscationForm.jsx

--- my-project/src/Components/TranscationForm.jsx
+++ my-project/src/Components/TranscationForm.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TranscationForm = () => {
-  return <div>TranscationForm</div>;
+const TranscationForm = ({ addTransaction }) => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!text) {
+      alert("No data");
+      return;
+    }
+
+    addTransaction(text, amount);
+    setText("");
+    setAmount(0);
+  };
+  console.log(text);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            type="text"
+            value={text}
+            placeholder="ชื่อรายการ..."
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={amount}
+            placeholder="กรอกจำนวนเงิน"
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default TranscationForm;
